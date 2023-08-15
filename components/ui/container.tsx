e78d5eebde536df8d37079021eefcb09d4804970
Kevin Peng <kpeng66@gmail.com>
2023-08-15 11:18:43 -0700

    finished store features

---
+++ components/ui/container.tsx
@@ -0,0 +1,15 @@
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({
+    children
+}) => {
+    return (
+        <div className="mx-auto max-3-7xl">
+            {children}
+        </div>
+    );
+}
+
+export default Container
